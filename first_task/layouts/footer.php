6e2af25880b3c3ff848199b25edd08be8f6f2297
rensousadev <dev.register.api@gmail.com>
2025-10-14 22:36:40 -0300

    feat: implement comprehensive footer layout
    - Add 4-column footer layout with responsive design
    - Include company branding and description section
    - Add main navigation links (Destaques)
    - Implement contact information section
    - Add Instagram product showcase gallery
    - Include social media icons (Facebook, Instagram, Twitter)
    - Add copyright and Bootstrap JavaScript integration

---
+++ first_task/layouts/footer.php
@@ -0,0 +1,60 @@
+<footer class="mt-5 py-5 bg-dark text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="assets/imgs/logo.svg" alt="Logo" class="img-fluid mb-3" style="max-width: 100px; filter: brightness(0) invert(1);">
+                <p class="text-muted">Os melhores produtos pelo melhor preço.</p>
+            </div>
+            <div class="col-md-3">
+                <h5 class="text-white mb-3">Destaques</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-muted text-decoration-none">HOMENS</a></li>
+                    <li><a href="#" class="text-muted text-decoration-none">MULHERES</a></li>
+                    <li><a href="#" class="text-muted text-decoration-none">ACESSÓRIOS</a></li>
+                    <li><a href="#" class="text-muted text-decoration-none">VENDAS</a></li>
+                    <li><a href="#" class="text-muted text-decoration-none">PARCEIROS</a></li>
+                    <li><a href="#" class="text-muted text-decoration-none">CARREIRAS</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5 class="text-white mb-3">Fale conosco</h5>
+                <p class="text-muted mb-1">Email: site_email@.com</p>
+                <p class="text-muted mb-1">Tel: +55 (XX) XXXX-XXXX</p>
+                <p class="text-muted mb-1">Fax: +55 (XX) XXXX-XXXX</p>
+                <p class="text-muted">Em: shopping@gmai.com</p>
+            </div>
+            <div class="col-md-3">
+                <h5 class="text-white mb-3">Instagram</h5>
+                <div class="row g-2">
+                    <div class="col-4">
+                        <img src="assets/imgs/produto1.jpg" alt="Produto 1" class="img-fluid rounded">
+                    </div>
+                    <div class="col-4">
+                        <img src="assets/imgs/produto2.jpg" alt="Produto 2" class="img-fluid rounded">
+                    </div>
+                    <div class="col-4">
+                        <img src="assets/imgs/produto3.jpg" alt="Produto 3" class="img-fluid rounded">
+                    </div>
+                    <div class="col-4">
+                        <img src="assets/imgs/produto1.jpg" alt="Produto 4" class="img-fluid rounded">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="my-4 bg-secondary">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <p class="text-muted mb-0">eCommerce @ 2022 All Right Reserved</p>
+            </div>
+            <div class="col-md-6 text-end">
+                <a href="#" class="text-muted me-3"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="text-muted me-3"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="text-muted"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
